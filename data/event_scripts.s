80923253a7cb078b008680bbc2419e59671f6c32
nathan <nathangwooley@gmail.com>
2025-05-07 17:11:24 +1000

    Add Mt Rusturf and encounters, no events yet

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1171,3 +1171,5 @@ EventScript_VsSeekerChargingDone::
 	.include "data/maps/Upper_RusturfTunnel/scripts.inc"
 
 	.include "data/maps/Route116North/scripts.inc"
+
+	.include "data/maps/Mt_Rusturf/scripts.inc"

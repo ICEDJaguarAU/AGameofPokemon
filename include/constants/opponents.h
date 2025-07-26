8e767f04d3206a145f14ec16a6b53ab8b0e8d38d
unknown <nathangwooley@gmail.com>
2025-07-26 10:11:42 +1000

    Succesfully added Vito_Fallarbor and flyaway script?

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -85,7 +85,7 @@
 #define TRAINER_EDGAR                        79
 #define TRAINER_ALBERT                       80
 #define TRAINER_SAMUEL                       81
-#define TRAINER_VITO                         82
+#define TRAINER_VITO_FALLARBOR               82
 #define TRAINER_OWEN                         83
 #define TRAINER_WILTON_2                     84
 #define TRAINER_WILTON_3                     85

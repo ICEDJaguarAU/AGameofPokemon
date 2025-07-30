dfa043060d57df801a516761e6aad1d4cee851c6
unknown <nathangwooley@gmail.com>
2025-07-30 16:15:50 +1000

    Edited Trainers and encounters Route 114

--- src/data/pokemon/species_info/gen_4_families.h
+++ src/data/pokemon/species_info/gen_4_families.h
@@ -540,7 +540,7 @@ const struct SpeciesInfo gSpeciesInfoGen4[] =
         .growthRate = GROWTH_MEDIUM_SLOW,
         .eggGroups = MON_EGG_GROUPS(EGG_GROUP_WATER_1, EGG_GROUP_FIELD),
     #if P_UPDATED_ABILITIES >= GEN_9
-        .abilities = { ABILITY_TORRENT, ABILITY_NONE, ABILITY_COMPETITIVE },
+        .abilities = { ABILITY_TORRENT, ABILITY_SNOW_WARNING, ABILITY_SNOW_WARNING },
     #else
         .abilities = { ABILITY_TORRENT, ABILITY_NONE, ABILITY_DEFIANT },
     #endif
